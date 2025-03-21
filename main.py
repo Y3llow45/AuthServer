3dd8308e20d410064c90b10bbaa42427d78958a8
Борислав Христов <hristovborislav45@gmail.com>
2025-03-21 22:23:02 +0200

    Update server

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import bcrypt
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
 
 firebase_config = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),  # Ensure new lines
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
@@ -17,7 +18,7 @@ firebase_config = {
     "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
     "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
 }
-cred = credentials.Certificate(firebase_config)
+cred = credentials.Certificate(json.loads(json.dumps(firebase_config)))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -57,5 +58,7 @@ def login():
     else:
         return jsonify({"error": "Invalid password"}), 401
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
